5b0ceacc54803922d01e95e0ba0f9357c7c133f8
DAADrarat <gi70759@gmail.com>
2026-09-14 09:28:30 +0900

    init

---
+++ src/main/java/lx/project/project05/config/PjWebInitializer.java
@@ -0,0 +1,32 @@
+package lx.project.project05.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+
+public class PjWebInitializer implements WebApplicationInitializer{
+
+	
+		@Override
+		public void onStartup(ServletContext servletContext) {
+			AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+			rootContext.register(SpringConfig.class);
+			servletContext.addListener(new ContextLoaderListener(rootContext));
+			
+			AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+			webContext.register(WebMvcConfig.class);
+			
+			ServletRegistration.Dynamic sd = servletContext.addServlet("dispatcher", new DispatcherServlet(webContext));
+			sd.setLoadOnStartup(1);
+			sd.addMapping("/");
+			
+		}
+
+		
+		
+}
---
+++ src/main/java/lx/project/project05/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package lx.project.project05.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class WebMvcConfig {
+
+	@Bean
+	public InternalResourceViewResolver jspViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	} 
+	
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+}
